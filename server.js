ddc7e45567fb782dcb06be7b8c57467447b0e190
devverma3 <devverma2031@gmail.com>
2026-05-19 19:42:02 +0530

    setup MongoDB connection

---
+++ server.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+const app = require("./src/app")
+const connectToDB = require("./src/config/database")
+const dns = require("dns")
+
+dns.setServers(["8.8.4.4", "8.8.8.8"])
+
+connectToDB()
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000")
+})

2e9344f4af4e3e2da64b64fdcdcf59a77c5d1bcd
devverma3 <devverma2031@gmail.com>
2026-05-21 21:08:33 +0530

    created fronted elements

---
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -0,0 +1,56 @@
+import React,{useState} from 'react'
+import '../auth.form.scss'
+import { useNavigate,Link } from 'react-router';
+import { useAuth } from '../hooks/useAuth';
+
+const Login = () => {
+
+
+    const {loading, handleLogin} = useAuth()
+
+    const [email, setemail] = useState("")
+    const [password, setpassword] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        handleLogin({ email, password })
+    }
+
+    if(loading){
+        return (<main><h1>Loading...</h1></main>)
+    }
+
+
+  return (
+    <main>
+        <div className="form-container">
+            <h1>Login</h1>
+
+            <form onSubmit={handleSubmit}>
+
+                <div className="input-group">
+                    <label htmlFor="email">Email</label>
+                    <input
+                    onChange={(e)=>{setemail(e.target.value)}} 
+                    type="email" id="email" name='email' placeholder='Enter email address' />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">Password</label>
+                    <input
+                    onChange={(e)=>{setpassword(e.target.value)}}
+                    type="password" id="password" name='password' placeholder='Enter password' />
+                </div>
+
+                <button className = 'button primary-button'>Login</button>
+
+
+
+            </form>
+
+            <p>Don't have an account? <Link to={"/register"}>Register</Link></p>
+        </div>
+    </main>
+  )
+}
+
+export default Login
